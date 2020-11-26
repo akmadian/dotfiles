e398d34e3a964dc17aadd308c512e6c68b9714c2
akmadian <akmadian@gmail.com>
2020-11-26 00:54:23 -0800

    Update install script

--- install.sh
+++ install.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
-chmod a+rx ./scripts/nas.sh
-chmod a+rx ./scripts/ovpn.sh
+echo "Creating dotfiles src at ~/src/dotfiles"
+sudo mkdir -p ~/src/dotfiles
+
+echo "Copying dotfiles repo from descartes to dotfiles src"
+cp -r ~/descartes/src/dotfiles/* ~./src/dotfiles/
+
+echo "Creating symlinks"
+ln -sf ~/src/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/src/dotfiles/scripts/nas.sh ~/nas.sh
+ln -sf ~/src/dotfiles/scripts/ovpn.sh ~/ovpn.sh
+
+echo "Setting execution privileges for scripts"
+chmod a+rx ~/nas.sh
+chmod a+rx ~/ovpn.sh
