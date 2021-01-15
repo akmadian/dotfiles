9e82a9afcf33f20bbc6df35f575a42459c314f91
Ari Madian <akmadian@gmail.com>
2021-01-15 12:23:32 -0800

    Add switch audio output script, update gpg signing config

--- install.sh
+++ install.sh
@@ -10,3 +10,5 @@ ln -sf ~/dotfiles/custom.zsh-theme ~/.oh-my-zsh/themes/custom.zsh-theme
 ln -sf ~/dotfiles/.Xresources      ~/.Xresources
 ln -sf ~/dotfiles/git/.gitconfig   ~/.gitconfig
 
+chmod +x ~/dotfiles/scripts/swao.sh
+sudo ln -sf ~/dotfiles/scripts/swao.sh /bin/swao
