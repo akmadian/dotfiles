8dd15fdd04f006b0d1da25913e405b5abe852420
Ari Madian <akmadian@gmail.com>
2021-01-14 17:27:50 -0800

    Add accidentally removed install script, update gitconfig

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "Creating Symlinks"
+set -x
+
+ln -sf ~/dotfiles/.zshrc           ~/.zshrc
+ln -sf ~/dotfiles/.nvimrc          ~/.config/nvim/init.vim
+ln -sf ~/dotfiles/.tmux.conf       ~/.tmux.conf
+ln -sf ~/dotfiles/custom.zsh-theme ~/.oh-my-zsh/themes/custom.zsh-theme
+ln -sf ~/dotfiles/.Xresources      ~/.Xresources
+ln -sf ~/dotfiles/git/.gitconfig   ~/.gitconfig
+
