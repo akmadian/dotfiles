576e0aaf3dd33a4068d5350a8fe8207c8f7070de
Ari Madian <akmadian@gmail.com>
2021-03-01 21:24:52 -0800

    Update fish config, add - -> cd - abbr.. yesss.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,11 @@
-set -Ua fish_user_paths $HOME/repos/cse374-21wi-monorepo/scripts
+# Generic PATH append format:
+#   set -Ua fish_user_paths [location]
+
+set -Ua fish_user_paths $HOME/cs/CSE-374/scripts
 set -Ua fish_user_paths $HOME/.local/bin
 set -x GPG_TTY $TTY
 set -x TMPDIR "/tmp"
+set -U EDITOR nvim
 
 set fish_color_command white
 
@@ -15,12 +19,13 @@ alias orm="bash orm" # No idea why, but this script really wants to be run with 
 # Package Managers
 alias ai="sudo apt install"
 alias agi="sudo apt-get install"
+
 alias pi="sudo pacman -S"
 alias yi="yay -S"
 
 # General
 alias cls="clear"
-alias ll="ls -alhF"
+alias ll="ls -lahF"
 alias unpacktargz="tar xvzf"
 alias ssn="sudo shutdown now"
 alias sleep="systemctl suspend"
@@ -30,37 +35,29 @@ alias xcs="xclip -selection c"
 
 # Navigation
 alias ..="cd .."
-alias .2="cd ../.."
-alias .3="cd ../../../"
 alias cdd="cd ~/Downloads"
 alias q="exit"
+abbr -a -- - 'cd -' # Cause alias - doesn't work
 
 # TUI Apps
 alias reddit="tuir --enable-media"
 alias email="neomutt"
+alias emailpull="mbsync -a"
 
 function fish_prompt
     set_color normal
     set git_branch (git branch 2>/dev/null | sed -n '/\* /s///p')
-    set_color C9B091
-    echo -n (whoami)
-    set_color A6A780
-    echo -n '@'
-    set_color D3A080
-    echo -n (hostname)
-    set_color B1B666
-    echo -n ' '(prompt_pwd)' '
+
+    set_color C9B091; echo -n (whoami)
+    set_color A6A780; echo -n '@'
+    set_color D3A080; echo -n (hostname)
+    set_color B1B666; echo -n ' '(prompt_pwd)' '
     if [ "$git_branch" != "" ]
-        set_color A45479
-        echo -n 'git'
-        set_color CAD89A
-        echo -n ':('
-        set_color A45479
-        echo -n $git_branch
-        set_color CAD89A
-        echo -n ') '
+        set_color A45479; echo -n 'git'
+        set_color CAD89A; echo -n ':('
+        set_color A45479; echo -n $git_branch
+        set_color CAD89A; echo -n ') '
     end
-    set_color normal
-    echo -n '-> '
+    set_color normal; echo -n '-> '
 end
 
