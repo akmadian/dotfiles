6ef7be0d027f56a4e0662483b51c78829f05bc4d
Ari Madian <akmadian@gmail.com>
2021-03-10 17:46:36 -0800

    .local/bin cleanup, update orm so no stdout, add lots of features to mutt-daemon, attempt gm inbox cleanup

--- .local/bin/mutt-daemon.sh
+++ .local/bin/mutt-daemon.sh
@@ -1,18 +1,39 @@
 #!/usr/bin/sh
 # Checks for new mail every 5 minutes, if there is new mail, sends
 #   a notification with herbe
+eval __MAIL="/home/ari/.config/dwm/__MAIL"
+touch $__MAIL
+ALWAYS_NOTIFY=true
+
+MAIL() {
+  UNREAD_GMAIL=$(find ~/.local/share/mail/akmadian@gmail.com/INBOX/new -type f | wc -l)
+  UNREAD=$(find ~/.local/share/mail/ari@madian.co/INBOX/new -type f | wc -l)
+
+  FMTD=$(printf "📫 gm: %d co: %d" "$UNREAD_GMAIL" "$UNREAD")
+  echo "$FMTD" > $__MAIL
+}
 
 while true
 do
   UNREAD_GM=$(find ~/.local/share/mail/akmadian@gmail.com/INBOX/new -type f | wc -l)
   UNREAD_CO=$(find ~/.local/share/mail/ari@madian.co/INBOX/new -type f | wc -l)
+  echo "Current Unread - Gmail: $UNREAD_GM madian.co: $UNREAD_CO"
 
-  mbsync -a
+  echo "Fetching New Mail"
+  herbe "Fetching New Mail." &
+  mbsync -a &
+  wait
+  echo "Done."
   NEW_UNREAD_GM=$(find ~/.local/share/mail/akmadian@gmail.com/INBOX/new -type f | wc -l)
   NEW_UNREAD_CO=$(find ~/.local/share/mail/ari@madian.co/INBOX/new -type f | wc -l)
+  echo "New Unread - Gmail: $NEW_UNREAD_GM madian.co: $NEW_UNREAD_CO"
 
-  GM_DIFF=$NEW_UNREAD_GM-$UNREAD_GM
-  CO_DIFF=$NEW_UNREAD_CO-$UNREAD_CO
+  GM_DIFF=`expr $NEW_UNREAD_GM - $UNREAD_GM`
+  CO_DIFF=`expr $NEW_UNREAD_CO - $UNREAD_CO`
+  echo "Diffs - Gmail: $GM_DIFF madian.co: $CO_DIFF"
+
+  echo "Updating Status Bar"
+  MAIL
 
   if [ $GM_DIFF -gt 0 ] || [ $CO_DIFF -gt 0 ]; then
     NOTIF_TEXT="New Mail Received"
@@ -24,9 +45,10 @@ do
       NOTIF_CO="ari@madian.co ($CO_DIFF)"
     fi
 
-    herbe "$NOTIF_TEXT" "${NOTIF_GM:-}" "${NOTIF_CO:-}"
+    herbe "$NOTIF_TEXT" "${NOTIF_GM:-}" "${NOTIF_CO:-}" &
   fi
 
-  sleep 300
+  echo "Sleeping..."
+  sleep 600
 done
 
--- .local/bin/orm.sh
+++ .local/bin/orm.sh
@@ -2,4 +2,5 @@
 # Opens the git remote branch in brave
 
 eval url=$(git remote -v | head -n 1 | awk -F "@" '{print $2}' | awk -F " " '{print $1}' | sed 's/:/\//g' | sed 's/.git//g' | awk '{print "http://"$1}')
-brave $url
+$BROWSER $url 2>&1
+
